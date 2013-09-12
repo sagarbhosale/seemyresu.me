a77843899dbabc8d8992430b708f254703ea2f8f
Sagar Bhosale <sagarbhosale019@gmail.com>
2013-09-12 03:38:09 -0700

    for heroku

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'digest/sha1'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module SeemyresuMe
@@ -60,5 +60,11 @@ module SeemyresuMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
+
+    config.assets.compile = true
+
+
   end
 end
