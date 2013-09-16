2c295411c247aaa7c85291b91dcfd99fffeacea3
Sagar Bhosale <sagarbhosale019@gmail.com>
2013-09-15 17:14:18 -0700

    clean up
     Please enter the commit message for your changes. Lines starting

--- app/models/Constants.rb
+++
@@ -1,6 +0,0 @@
-module GlobalConstants
-  # also notice the call to 'freeze'
-  MailServiceURLs = {"gmail" => "gmail.com", "yahoo.co" => "mail.yahoo.com"}.freeze
-
-  appURL = "http://localhost:3000/".freeze
-end
--- app/models/basic.rb
+++
@@ -1,5 +0,0 @@
-class Basic < ActiveRecord::Base
-  attr_accessible :email, :firstname, :lastname, :mobile, :resume_id
-
-  belongs_to :resume
-end
